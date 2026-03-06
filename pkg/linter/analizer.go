bf1dfc67c705a01ec08da8e9c6e901352c3f6267
michealsytch <misha.sytch@mail.ru>
2026-03-06 20:53:29 +0300

    Заготовка для линтера и его вызов в main.go

--- pkg/linter/analizer.go
+++ pkg/linter/analizer.go
@@ -1,1 +1,37 @@
 package linter
+
+import (
+	"go/ast"
+	"golang.org/x/tools/go/analysis"
+	"log/slog"
+)
+
+// Analyzer определяет конфигурацию линтера для использования в основной программе.
+var Analyzer = &analysis.Analyzer{
+	Name: "loglint",
+	Doc:  `Описание`, //FIXME: написать нужно описание
+	Run:  run,
+}
+
+var checkArg func(arg ast.Expr)
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	for _, file := range pass.Files {
+		ast.Inspect(file, func(n ast.Node) bool {
+			call, ok := n.(*ast.CallExpr)
+			if !ok || len(call.Args) == 0 {
+				return true
+			}
+
+			checkArg = func(arg ast.Expr) {
+				slog.Error("Оно не работает пока!")
+			}
+
+			for _, arg := range call.Args {
+				checkArg(arg)
+			}
+			return true
+		})
+	}
+	return nil, nil
+}
