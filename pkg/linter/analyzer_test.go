17c546c39a7c73d0652e791e69386c8bae0ac7fb
michealsytch <misha.sytch@mail.ru>
2026-03-06 20:48:00 +0300

    запуск теста

--- pkg/linter/analyzer_test.go
+++ pkg/linter/analyzer_test.go
@@ -1,1 +1,11 @@
 package linter
+
+import (
+	"golang.org/x/tools/go/analysis/analysistest"
+	"testing"
+)
+
+func TestAll(t *testing.T) {
+	testdata := analysistest.TestData()
+	analysistest.Run(t, testdata, Analyzer, "test_case")
+}
