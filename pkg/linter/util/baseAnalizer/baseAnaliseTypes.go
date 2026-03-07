20ac31ce98d9478eed9590519cdbc1f3a93a405b
michealsytch <misha.sytch@mail.ru>
2026-03-07 14:14:05 +0300

    Рефакторинг x2

--- pkg/linter/util/baseAnalizer/baseAnaliseTypes.go
+++ pkg/linter/util/baseAnalizer/baseAnaliseTypes.go
@@ -1,40 +1,11 @@
 package baseAnalizer
 
-import (
-	"go/ast"
-	"go/token"
-	"go/types"
-	"golang.org/x/tools/go/analysis"
-)
-
-// Reporter интерфейс заменяет стандартный *analysis.Pass и используется
-// благодаря утиной типизации
-type Reporter interface {
-	// Reportf регистрирует предупреждение в месте pos с форматированным сообщением
-	Reportf(pos token.Pos, format string, args ...interface{})
-
-	// Report регистрирует предупреждение в месте pos
-	Report(d analysis.Diagnostic)
-
-	// TypesInfo возвращает информацию о типах узлов
-	TypesInfo() *types.Info
+type analysisResult struct {
+	originalMsg string
+	fixedMsg    string
+	errors      []string
 }
 
-// PassWrapper обертка над analysis.Pass
-type PassWrapper struct {
-	*analysis.Pass
-}
-
-func (w PassWrapper) Report(d analysis.Diagnostic) {
-	w.Pass.Report(d)
-}
-
-func (w PassWrapper) TypesInfo() *types.Info {
-	return w.Pass.TypesInfo
-}
-
-var _ Reporter = (*PassWrapper)(nil)
-
-type errorsChecker func(pass Reporter, arg ast.Expr, msg string)
+type errorsChecker func(res *analysisResult)
 type textErrors map[string]bool
-type textSyntaxChecker func(pass Reporter, arg ast.Expr, errors textErrors, r rune)
+type textSyntaxChecker func(res *analysisResult, r rune, errors textErrors) bool
