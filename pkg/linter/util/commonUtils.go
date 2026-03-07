e057c98a57712acfd5e99c946f148b3003233d73
michealsytch <misha.sytch@mail.ru>
2026-03-07 10:39:24 +0300

    Добавлены функции распознавания контекста вызова функции

---
+++ pkg/linter/util/commonUtils.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"go/ast"
+	"linter/pkg/linter/model"
+)
+
+// IsFmtSprintf проверяет, является ли выражение вызовом функции fmt.Sprintf()
+func IsFmtSprintf(call *ast.CallExpr) bool {
+	selector, ok := call.Fun.(*ast.SelectorExpr)
+	if !ok {
+		return false
+	}
+	x, ok := selector.X.(*ast.Ident)
+	return ok && x.Name == "fmt" && selector.Sel.Name == "Sprintf"
+}
+
+// IsLogCall определяет, является ли вызов функции обращением к одному из
+// поддерживаемых логгеров, указанных в model.LoggerList
+func IsLogCall(call *ast.CallExpr) bool {
+	selector, ok := call.Fun.(*ast.SelectorExpr)
+	if !ok {
+		return false
+	}
+	x, ok := selector.X.(*ast.Ident)
+	if !ok {
+		return false
+	}
+
+	for _, loggerName := range model.LoggerList {
+		if x.Name == loggerName {
+			return true
+		}
+	}
+
+	return false
+}
