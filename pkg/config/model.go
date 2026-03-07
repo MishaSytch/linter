65238235d82f6c58fcf1f309a43cf4c78686dee9
michealsytch <misha.sytch@mail.ru>
2026-03-07 14:16:05 +0300

    В конфиге можно управлять выводом репорта

--- pkg/config/model.go
+++ pkg/config/model.go
@@ -1,6 +1,12 @@
 package config
 
 type Config struct {
-	Loggers        []string `yaml:"loggers"`
-	SensitiveWords []string `yaml:"sensitive_words"`
+	Loggers        []string     `yaml:"loggers"`
+	SensitiveWords []string     `yaml:"sensitive_words"`
+	Output         OutputConfig `yaml:"output"`
+}
+
+type OutputConfig struct {
+	ShowInConsole   bool `yaml:"show_in_console"`
+	ShowSuggestions bool `yaml:"show_suggestions"`
 }
